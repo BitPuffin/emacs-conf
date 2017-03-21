b27f1cf9a5f1a63cf4b6f8e3046c9688104390f4
Isak Andersson <contact@bitpuffin.com>
2017-03-21 06:56:58 +0100

    auto install packages

--- pkgs.el
+++ pkgs.el
@@ -6,3 +6,9 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
+
+
+(unless (seq-every-p #'package-installed-p
+                       package-selected-packages)
+    (package-refresh-contents)
+    (package-install-selected-packages))
