2ab987097b4c4b6316f5ae0562ae483f7dead693
contact <contact@bitpuffin.com>
2017-03-21 03:37:32 +0000

    remove test code that I forgot about

--- cnfcustom.el
+++ cnfcustom.el
@@ -2,7 +2,5 @@
 ;;; Customize Emacs Custom mechanism
 ;;;
 
-(file-name-directory "/bla/yo/ey.el")
-
 (setq custom-file (concat (file-name-directory load-file-name) "AUTO-custom.el"))
 (load custom-file)
