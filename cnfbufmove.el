5bb0808938c27370dd98a4eea3464cfe2e76298f
contact <contact@bitpuffin.com>
2017-03-21 05:46:36 +0000

    actually now it is configured, this file didn't get included

---
+++ cnfbufmove.el
@@ -0,0 +1,8 @@
+;;;
+;;; Configure buffer-move
+;;;
+
+(global-set-key (kbd "<S-left>") 'buf-move-left)
+(global-set-key (kbd "<S-right>") 'buf-move-right)
+(global-set-key (kbd "<S-down>") 'buf-move-down)
+(global-set-key (kbd "<S-up>") 'buf-move-up)
