b71ec0a2cbd50ad8f8775459197667847b4115b3
contact <contact@bitpuffin.com>
2017-03-21 04:21:34 +0000

    Configure ido-at-point

--- cmpltn.el
+++ cmpltn.el
@@ -2,7 +2,12 @@
 ;;; Completion configuration
 ;;;
 
+(autoload 'ido-at-point-mode "ido-at-point")
+
+
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode t)
 (global-set-key (kbd "M-x") 'smex)
+
+(ido-at-point-mode)
