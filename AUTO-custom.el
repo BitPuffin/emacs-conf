2d297b8624e47b1a5796719a583b300b1fcb19d6
Isak Andersson <contact@bitpuffin.com>
2017-08-21 19:19:57 +0200

    customize eval print length

--- AUTO-custom.el
+++ AUTO-custom.el
@@ -8,9 +8,11 @@
    (quote
     (".cm/" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".dll" ".pdb" ".mdb" ".so" ".dylib")))
  '(cursor-type (quote box))
+ '(eval-expression-print-length nil)
  '(geiser-active-implementations (quote (chibi)))
  '(ido-create-new-buffer (quote always))
  '(ido-ignore-extensions t)
+ '(ispell-program-name "aspell")
  '(package-selected-packages
    (quote
     (magit buffer-move yasnippet visual-regexp browse-kill-ring smart-region ido-at-point darcsum geiser adoc-mode julia-shell julia-mode nim-mode fsharp-mode csv-mode rust-mode paredit racket-mode circe twittering-mode switch-window sml-mode smex slime lua-mode actionscript-mode)))
