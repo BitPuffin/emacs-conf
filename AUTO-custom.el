0fe7e534841ceb90b18bde930eb5e4198b7df3ac
Isak Andersson <contact@bitpuffin.com>
2017-08-25 11:24:00 +0200

    set up pretty-mode (turns various symbols into pretty symbols)

--- AUTO-custom.el
+++ AUTO-custom.el
@@ -4,18 +4,46 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote eww-browse-url))
+ '(c++-mode-hook (quote (turn-on-pretty-mode)))
+ '(c-mode-hook (quote (macrostep-c-mode-hook turn-on-pretty-mode)))
  '(completion-ignored-extensions
    (quote
     (".cm/" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".dll" ".pdb" ".mdb" ".so" ".dylib")))
  '(cursor-type (quote box))
+ '(emacs-lisp-mode-hook
+   (quote
+    (ert--activate-font-lock-keywords
+     (lambda nil
+       (set
+        (make-local-variable
+         (quote indent-tabs-mode))
+        nil))
+     turn-on-pretty-mode)))
  '(eval-expression-print-length nil)
  '(geiser-active-implementations (quote (chibi)))
  '(ido-create-new-buffer (quote always))
  '(ido-ignore-extensions t)
  '(ispell-program-name "aspell")
+ '(java-mode-hook (quote (turn-on-pretty-mode)))
+ '(lisp-interaction-mode-hook (quote (turn-on-pretty-mode)))
+ '(lisp-mode-hook
+   (quote
+    (common-lisp-lisp-mode-hook
+     (lambda nil
+       (add-hook
+        (quote font-lock-extend-region-functions)
+        (quote slime-extend-region-for-font-lock)
+        t t))
+     (lambda nil
+       (set
+        (make-local-variable
+         (quote indent-tabs-mode))
+        nil))
+     slime-lisp-mode-hook turn-on-pretty-mode)))
+ '(objc-mode-hook (quote (turn-on-pretty-mode)))
  '(package-selected-packages
    (quote
-    (package-build shut-up epl git commander f dash s feature-mode magit buffer-move yasnippet visual-regexp browse-kill-ring smart-region ido-at-point darcsum geiser adoc-mode julia-shell julia-mode nim-mode fsharp-mode csv-mode rust-mode paredit racket-mode circe twittering-mode switch-window sml-mode smex slime lua-mode actionscript-mode)))
+    (pretty-mode package-build shut-up epl git commander f dash s feature-mode magit buffer-move yasnippet visual-regexp browse-kill-ring smart-region ido-at-point darcsum geiser adoc-mode julia-shell julia-mode nim-mode fsharp-mode csv-mode rust-mode paredit racket-mode circe twittering-mode switch-window sml-mode smex slime lua-mode actionscript-mode)))
  '(safe-local-variable-values (quote ((rust-indent-offset . 2)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
