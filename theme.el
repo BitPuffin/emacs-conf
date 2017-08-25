0530149a41503ec1ab73dff19c009257038d59cf
Isak Andersson <contact@bitpuffin.com>
2017-08-25 16:42:40 +0200

    With pretty mode it's nicer to have slightly larger fonts

--- theme.el
+++ theme.el
@@ -6,6 +6,6 @@
 
 ; Retina
 (if (eq system-type 'darwin)
-	(set-face-attribute 'default nil :font "DejaVu Sans Mono-16")
-	(set-face-attribute 'default nil :font "DejaVu Sans Mono-8"))
+	(set-face-attribute 'default nil :font "DejaVu Sans Mono-18")
+	(set-face-attribute 'default nil :font "DejaVu Sans Mono-10"))
 
